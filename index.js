05a00ad458d0b7bc656d1429153b88bdc1fe4ac6
acidrums <edoardo.carradori.96@gmail.com>
2023-03-02 17:33:30 +0100

    ES6 (Front End JS) vs NodeJS

--- index.js
+++ index.js
@@ -1,8 +1,38 @@
+// in nodejs
+// require()
+
+// in front-end js you cant use require
+// import
+import { ethers } from "./ethers-5.6.esm.min.js"
+
+const connectButton = document.getElementById("connectButton")
+const fundButton = document.getElementById("fundButton")
+connectButton.onclick = connect
+fundButton.onclick = fund
+
+console.log(ethers)
+
 async function connect() {
     if (window.ethereum !== undefined) {
         await window.ethereum.request({ method: "eth_requestAccounts" })
-        document.getElementById("connectButton").innerHTML = "Connected!"
+        connectButton.innerHTML = "Connected!"
     } else {
-        document.getElementById("connectButton").innerHTML = "Please install Metamask!"
+        connectButton.innerHTML = "Please install Metamask!"
     }
 }
+
+async function fund(ethAmount) {
+    console.log(`Funding with ${ethAmount}...`)
+    if (window.ethereum !== undefined) {
+        // provider / connection to the Blockchain
+        // signer / wallet / someone with some gas
+
+        // contract that we are interacting with
+        // ^ ABI & Address
+
+    }
+}
+
+// fund function
+
+// withdraw
