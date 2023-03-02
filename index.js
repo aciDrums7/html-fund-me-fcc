cfa8c33d372dd2bdf9b975ceadf57f96cda58bdb
Edoardo Carradori <edoardo.carradori.96@gmail.com>
2023-03-02 22:14:29 +0100

    Withdraw Function

--- index.js
+++ index.js
@@ -9,9 +9,11 @@ import { abi, contractAddress } from "./constants.js"
 const connectButton = document.getElementById("connectButton")
 const fundButton = document.getElementById("fundButton")
 const balanceButton = document.getElementById("balanceButton")
+const withdrawButton = document.getElementById("withdrawButton")
 connectButton.onclick = connect
 fundButton.onclick = fund
 balanceButton.onclick = getBalance
+withdrawButton.onclick = withdraw
 
 console.log(ethers)
 
@@ -72,4 +74,18 @@ function listenForTransactionMine(transactionResponse, provider) {
     // create a listener for the blockchain
 }
 
-// withdraw
+async function withdraw() {
+    if (window.ethereum !== undefined) {
+        console.log('Withdrawing...')
+        const provider = new ethers.providers.Web3Provider(window.ethereum)
+        const signer = provider.getSigner()
+        console.log(signer)
+        const contract = new ethers.Contract(contractAddress, abi, signer)
+        try {
+            const transactionResponse = await contract.withdraw()
+            await listenForTransactionMine(transactionResponse, provider)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+}
