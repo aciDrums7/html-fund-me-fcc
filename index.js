a8740dbf57c305ce8fade17a5190945ed376653b
acidrums <edoardo.carradori.96@gmail.com>
2023-03-02 18:47:55 +0100

    Sending a Transaction from a Website

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 // in front-end js you cant use require
 // import
 import { ethers } from "./ethers-5.6.esm.min.js"
+import { abi, contractAddress } from "./constants.js"
 
 const connectButton = document.getElementById("connectButton")
 const fundButton = document.getElementById("fundButton")
@@ -21,7 +22,8 @@ async function connect() {
     }
 }
 
-async function fund(ethAmount) {
+async function fund() {
+    const ethAmount = "0.1"
     console.log(`Funding with ${ethAmount}...`)
     if (window.ethereum !== undefined) {
         // provider / connection to the Blockchain
@@ -29,7 +31,17 @@ async function fund(ethAmount) {
 
         // contract that we are interacting with
         // ^ ABI & Address
-
+        const provider = new ethers.providers.Web3Provider(window.ethereum)
+        const signer = provider.getSigner()
+        console.log(signer)
+        const contract = new ethers.Contract(contractAddress, abi, signer)
+        try {
+            const transactionResponse = await contract.fund({
+                value: ethers.utils.parseEther(ethAmount)
+            })
+        } catch(error) {
+            console.log(error)
+        }
     }
 }
 
